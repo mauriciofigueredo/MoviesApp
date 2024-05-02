36dad63241095f2031ac13e269e60b9eb274e998
Mauricio Figueredo <mauriciofigueredo46@gmail.com>
2024-05-02 16:16:12 -0300

    Initial project, before reset cahe

--- App.tsx
+++ App.tsx
@@ -1,20 +1,16 @@
+import 'react-native-gesture-handler';
+
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { HomeScreen } from './src/presentation/screens/home/HomeScreen';
+import { NavigationContainer } from '@react-navigation/native';
+import { StackNavigator } from './src/presentation/navigation/StackNavigator';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <StackNavigator />
+    </NavigationContainer>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
